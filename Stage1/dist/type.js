0ab67c2edc50753395a0d0f8c8204f9508b06b02
MewPK <milzaza123@gmail.com>
2022-12-23 00:40:34 +0700

    type update!

--- Stage1/dist/type.js
+++ Stage1/dist/type.js
@@ -1,3 +1,4 @@
 "use strict";
-const Jack = { name: "Jackky", age: 20 };
+const Jack = { name: "Jackky", age: 20, status: "ok" };
+const Chom = { name: "Jackky", age: 20, phone: "093-211-3221", status: "ok" };
 console.log(Jack);
--- Stage1/src/type.ts
+++ Stage1/src/type.ts
@@ -1,6 +1,9 @@
 type Person = {
     name : string,
     age : number
+    phone ?: string
+    readonly status : string
 }
-const Jack : Person = {name : "Jackky"  , age : 20};
+const Jack : Person = {name : "Jackky"  , age : 20 , status : "ok"};
+const Chom : Person = {name : "Jackky"  , age : 20 , phone : "093-211-3221" , status : "ok" };
 console.log(Jack)
