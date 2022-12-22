dc0f765e4d6f71365de04767aa46b7168b3d0714
MewPK <milzaza123@gmail.com>
2022-12-22 21:59:49 +0700

    Created function with typescript !

---
+++ Stage1/1.js
@@ -0,0 +1,5 @@
+var total = function (a, b) {
+    return a + b;
+};
+console.log(total(100, 200));
+// console.log(total("10","20"))
---
+++ Stage1/1.ts
@@ -0,0 +1,6 @@
+const total = (a:number , b:number) =>{
+    return a+b;
+}
+
+console.log(total(100,200))
+// console.log(total("10","20"))
