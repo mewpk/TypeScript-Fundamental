e9a355cd4ef2c9b00fade21c2b98f265909dafa3
MewPK <milzaza123@gmail.com>
2022-12-22 22:07:12 +0700

    edit tsconfig.json

--- Stage1/1.js
+++
@@ -1,5 +0,0 @@
-var total = function (a, b) {
-    return a + b;
-};
-console.log(total(100, 200));
-// console.log(total("10","20"))
--- Stage1/1.ts
+++
@@ -1,6 +0,0 @@
-const total = (a:number , b:number) =>{
-    return a+b;
-}
-
-console.log(total(100,200))
-// console.log(total("10","20"))
