55673527c3936daa8eebbd49718de25f258f987a
MewPK <milzaza123@gmail.com>
2022-12-23 00:35:44 +0700

    add file type.ts

---
+++ Stage1/dist/type.js
@@ -0,0 +1,3 @@
+"use strict";
+const Jack = { name: "Jackky", age: 20 };
+console.log(Jack);
---
+++ Stage1/src/type.ts
@@ -0,0 +1,6 @@
+type Person = {
+    name : string,
+    age : number
+}
+const Jack : Person = {name : "Jackky"  , age : 20};
+console.log(Jack)
